782582619a40d566917cc9f412b3d56752361373
Jan Patrick P. Macasero <janpatrick0601@gmail.com>
2015-02-18 10:47:41 +0800

    update again.....

--- Practice/jpmacasero/database/it111_excercise1.sql
+++ Practice/jpmacasero/database/it111_excercise1.sql
@@ -2,13 +2,11 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `it111_excercise` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `it111_excercise` ;
 
 -- -----------------------------------------------------
--- Table `it111_excercise`.`region`
+-- Table `it111_excercise1`.`region`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `it111_excercise`.`region` (
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`region` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `region_code` VARCHAR(32) NULL,
   `region_description` VARCHAR(32) NULL,
@@ -17,9 +15,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `it111_excercise`.`province`
+-- Table `it111_excercise1`.`province`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `it111_excercise`.`province` (
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`province` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `province_code` VARCHAR(32) NULL,
   `province_description` VARCHAR(32) NULL,
@@ -28,16 +26,16 @@ CREATE TABLE IF NOT EXISTS `it111_excercise`.`province` (
   INDEX `fk_Province_Region_idx` (`Region_id` ASC),
   CONSTRAINT `fk_Province_Region`
     FOREIGN KEY (`Region_id`)
-    REFERENCES `it111_excercise`.`region` (`id`)
+    REFERENCES `it111_excercise1`.`region` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `it111_excercise`.`city`
+-- Table `it111_excercise1`.`city`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `it111_excercise`.`city` (
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`city` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `city_code` VARCHAR(32) NULL,
   `city_description` VARCHAR(32) NULL,
@@ -46,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `it111_excercise`.`city` (
   INDEX `fk_City_Province1_idx` (`Province_id` ASC),
   CONSTRAINT `fk_City_Province1`
     FOREIGN KEY (`Province_id`)
-    REFERENCES `it111_excercise`.`province` (`id`)
+    REFERENCES `it111_excercise1`.`province` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
