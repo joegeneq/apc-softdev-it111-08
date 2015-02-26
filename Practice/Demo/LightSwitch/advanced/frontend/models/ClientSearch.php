8c436b6f414f3f1034118544394dd231e6153a38
Ave Jay Moradillo <reikaxkira@gmail.com>
2015-02-26 08:47:17 +0800

    Demo

---
+++ Practice/Demo/LightSwitch/advanced/frontend/models/ClientSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Client;
+
+/**
+ * ClientSearch represents the model behind the search form about `app\models\Client`.
+ */
+class ClientSearch extends Client
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['client_name', 'address', 'company_name', 'company_position'], 'safe'],
+            [['contact_number'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Client::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'contact_number' => $this->contact_number,
+        ]);
+
+        $query->andFilterWhere(['like', 'client_name', $this->client_name])
+            ->andFilterWhere(['like', 'address', $this->address])
+            ->andFilterWhere(['like', 'company_name', $this->company_name])
+            ->andFilterWhere(['like', 'company_position', $this->company_position]);
+
+        return $dataProvider;
+    }
+}
---
+++ Practice/Demo/LightSwitch/advanced/frontend/views/site/signup.php
@@ -0,0 +1,35 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+<div class="site-signup">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to sign up:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                <?= $form->field($model, 'email')->hint('Please enter your email address')->label('Email:') ?>
+				<?= $form->field($model, 'username')->label('Username:') ?>
+				<?= $form->field($model, 'firstname')->label('Firstname:') ?>
+				<?= $form->field($model, 'lastname') ?>
+				<?= $form->field($model, 'roles')->dropDownList(['User','Admin'], ['prompt'=>'Select...'])->label('Account Type') ?>
+                
+				<?= $form->field($model, 'password')->passwordInput() ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
