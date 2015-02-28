a68be2072bac1e77ac3235bb3209f5ae3a203c1c
Miguel Carlo Alonzo <malonzo16@gmail.com>
2015-02-28 10:45:50 +0800

    connected ourspace to db

--- Practice/midterm_test/Alonzo_Miguel_Carlo_midterms/ourspace/config/db.php
+++ Practice/midterm_test/Alonzo_Miguel_Carlo_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_alonzo_miguel_carlo_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
