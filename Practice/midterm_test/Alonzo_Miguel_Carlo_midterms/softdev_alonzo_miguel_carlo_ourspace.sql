a558b0cf3bc444726a6bacec52b62147689a48cc
Miguel Carlo Alonzo <malonzo16@gmail.com>
2015-03-02 02:08:31 +0800

    Inserted one record for myaddress and mycomment

--- Practice/midterm_test/Alonzo_Miguel_Carlo_midterms/softdev_alonzo_miguel_carlo_ourspace.sql
+++ Practice/midterm_test/Alonzo_Miguel_Carlo_midterms/softdev_alonzo_miguel_carlo_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 11:18 AM
+-- Generation Time: Mar 02, 2015 at 02:07 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -39,7 +39,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `id_2` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'Miguel Carlo', 'Reyes', 'Alonzo', 'M', '2015-03-01 18:03:06', '123 street', '1234567', '987654321');
 
 -- --------------------------------------------------------
 
@@ -55,7 +62,14 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `myaddress_id` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(1, 1, 'Miguel Alonzo', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '2015-03-01 18:06:44');
 
 --
 -- Constraints for dumped tables
