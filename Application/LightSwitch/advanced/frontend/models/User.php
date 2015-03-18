fc3e010b001aa73d1aebcfb5fe05b1a0dad52a5b
Miguel Carlo Alonzo <malonzo16@gmail.com>
2015-03-18 11:34:00 +0800

    User model
    Generate User model and CRUD

---
+++ Application/LightSwitch/advanced/frontend/models/User.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property string $roles
+ * @property integer $company
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property AuthAssignment[] $authAssignments
+ * @property AuthItem[] $itemNames
+ * @property AuthItem $roles0
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'firstname', 'lastname', 'auth_key', 'password_hash', 'email', 'roles', 'company', 'created_at', 'updated_at'], 'required'],
+            [['company', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+            [['firstname', 'lastname'], 'string', 'max' => 100],
+            [['auth_key'], 'string', 'max' => 32],
+            [['roles'], 'string', 'max' => 64],
+            [['roles'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+            'auth_key' => 'Auth Key',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'roles' => 'Roles',
+            'company' => 'Company',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthAssignments()
+    {
+        return $this->hasMany(AuthAssignment::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemNames()
+    {
+        return $this->hasMany(AuthItem::className(), ['name' => 'item_name'])->viaTable('auth_assignment', ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRoles0()
+    {
+        return $this->hasOne(AuthItem::className(), ['name' => 'roles']);
+    }
+}
